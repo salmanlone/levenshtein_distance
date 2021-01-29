594dc9918f6daf93ec61461414aff84b2b4f0554
salmanlone2020 <63392556+salmanlone2020@users.noreply.github.com>
2021-01-29 15:50:28 +0800

    routing of components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
+import { ErrorComponent } from './error/error.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "home", component: HomeComponent
+  },
+  {
+    path: "about", component: AboutComponent
+  },
+  {
+    path: "", component: HomeComponent, pathMatch: "full"
+  },
+  {
+    path: "**", component: ErrorComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
