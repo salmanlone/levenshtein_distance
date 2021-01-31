04e931a3dc08727673839759b711520d2d9c51cc
salmanlone2020 <63392556+salmanlone2020@users.noreply.github.com>
2021-01-31 17:08:48 +0800

    environment variables added

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  api_url: "https://localhost:44396/compute",
+  secretKey: "0f44443b-bb32-4d37-9f27-082cec554b6a"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  api_url: "https://localhost:44396/compute",
+  secretKey: "0f44443b-bb32-4d37-9f27-082cec554b6a"
 };
 
 /*
